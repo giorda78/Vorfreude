85a773e2bf1204fb0abb57363700e2d12d18a6ce
Lucas Giorda <lucasgiorda78@gmail.com>
2021-09-22 15:07:55 -0300

    Se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.nav-main' , {delay: 100});
+  ScrollReveal().reveal('.showcase', {delay: 500});
+  ScrollReveal().reveal('.features', {delay: 500});
+  ScrollReveal().reveal('.presentacion', { delay: 500 });
+  ScrollReveal().reveal('.news-cards', { delay: 500 });
+  ScrollReveal().reveal('.tituloCat', {delay: 500});
+  ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+  ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
